af4ba52081a36356f107340c37c898d537721ec5
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-06-07 20:08:04 +0200

    Clean up code

--- src/components/Card.spec.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Card from './Card'
-
-describe('Card', () => {
-  it('renders its children', () => {
-    render(<Card>Hello world</Card>)
-    expect(screen.getByText('Hello world')).toBeInTheDocument()
-  })
-})
