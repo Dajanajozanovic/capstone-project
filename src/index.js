bd65631f372bdf9d380066ab1eac5f73b56fd09f
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-06-15 16:40:52 +0200

    add react-router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 import GlobalStyle from './GlobalStyles'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
