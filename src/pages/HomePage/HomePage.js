23eed1abf55361d900060be47478db928775f735
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-06-15 16:38:43 +0200

    add HomePage

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,50 @@
+import styled from 'styled-components/macro'
+import Header from './../../components/Header/Header'
+import Button from './../../components/Button/Button'
+import PropTypes from 'prop-types'
+
+HomePage.propTypes = {
+  onNavigate: PropTypes.func.isRequired,
+}
+
+export default function HomePage({ onNavigate }) {
+  return (
+    <Wrapper>
+      <Header>App title</Header>
+      <PageButtons>
+        <Button onClick={onNavigate}>Sightseeing</Button>
+        <Button onClick={onNavigate}>Foods and Drinks</Button>
+      </PageButtons>
+      <div>
+        <Image src="https://media-cdn.tripadvisor.com/media/photo-s/02/5a/ac/e6/ashoka.jpg" />
+        <Image src="https://www.handelsblatt.com/images/container-terminal-altenwerder/26116210/2-format2020.jpg" />
+      </div>
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`
+
+const Image = styled.img`
+  width: 300px;
+  height: 300px;
+  margin: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
+`
+const PageButtons = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 20px;
+`
