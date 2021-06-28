d91196e961b488599169a4abe02b2d5c208d677e
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-06-28 15:17:38 +0200

    add a new nav structure

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,10 @@
 import PropTypes from 'prop-types'
 import { NavLink } from 'react-router-dom'
 import styled from 'styled-components/macro'
+import naviIconsSightseeing from './../../images/naviIcons/sightseeing.png'
+import naviIconsRestaurants from './../../images/naviIcons/restaurants.png'
+import naviIconsFavorites from './../../images/naviIcons/heart.png'
+import naviIconsHome from './../../images/naviIcons/home.png'
 
 Navigation.propTypes = {
   pages: PropTypes.arrayOf(
@@ -8,22 +12,41 @@ Navigation.propTypes = {
   ),
 }
 
-export default function Navigation({ pages }) {
+export default function Navigation() {
   return (
     <Nav>
-      {pages.map(({ title, path }) => (
-        <StyledNavLink
-          activeStyle={{ backgroundColor: 'lightpink' }}
-          key={title}
-          exact
-          to={path}
-        >
-          {title}
-        </StyledNavLink>
-      ))}
+      <StyledNavLink exact to="/" activeClassName="active">
+        <img src={naviIconsHome} alt="" />
+      </StyledNavLink>
+      <StyledNavLink exact to="/sightseeing" activeClassName="active">
+        <img src={naviIconsSightseeing} alt="" />
+      </StyledNavLink>
+      <StyledNavLink exact to="/restaurants" activeClassName="active">
+        <img src={naviIconsRestaurants} alt="" />
+      </StyledNavLink>
+      <StyledNavLink exact to="/myfavorites" activeClassName="active">
+        <img src={naviIconsFavorites} alt="" />
+      </StyledNavLink>
     </Nav>
   )
 }
+
+// export default function Navigation({ pages }) {
+//   return (
+//     <Nav>
+//       {pages.map(({ title, path }) => (
+//         <StyledNavLink
+//           activeStyle={{ backgroundColor: 'lightpink' }}
+//           key={title}
+//           exact
+//           to={path}
+//         >
+//           {title}
+//         </StyledNavLink>
+//       ))}
+//     </Nav>
+//   )
+// }
 
 const Nav = styled.nav`
   height: 4rem;
@@ -31,6 +54,10 @@ const Nav = styled.nav`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
   justify-items: center;
+  img {
+    width: 32px;
+    height: auto;
+  }
 `
 
 const StyledNavLink = styled(NavLink)`
