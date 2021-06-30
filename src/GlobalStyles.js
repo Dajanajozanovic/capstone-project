dbc9be5232a4a989b13664adcededf179ec3ef9c
Dajana Jozanovic <dajana.jozanovic@t-online.de>
2021-06-30 11:29:30 +0200

    add root to Glebalstyles

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -5,6 +5,22 @@ export default createGlobalStyle`
 * {
     box-sizing: border-box;
 }
+
+
+
+:root{
+--color-primary: #304269;
+--color-primary-background: #91BED4;
+--color-secondary: #FFFFFF;
+--color-secondary-background: ##F26101;
+--color-active: whitesmoke;
+--color-active-background: #F26101;
+--color-navigation-hover: #91BED4;
+--color-navigation-background: #D9E8F5;
+--color-heade-background: #D9E8F5;
+}
+
+
 body {
     margin: 0;
     font-family: 'Benne', serif;
